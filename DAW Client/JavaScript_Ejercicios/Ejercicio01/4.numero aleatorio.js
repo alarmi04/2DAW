b593024a81c25419b4ddb3b465aa6c826f7da333
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2025-09-13 22:59:35 +0200

    Ejercicio 4 numero aleatorio finalizado

--- DAW Client/JavaScript_Ejercicios/Ejercicio01/4.numero aleatorio.js
+++ DAW Client/JavaScript_Ejercicios/Ejercicio01/4.numero aleatorio.js
@@ -1,13 +1,37 @@
 let numero;
-const numeroAadivinar = Math.random() * 11;
+const numeroAadivinar = parseInt(Math.random() * 10) + 1;
 let contador = 0;
+let seguirJugando = false;
 do {
   numero = parseInt(
     prompt("¿En qué número entero del 1 al 10 estoy pensando?")
   );
   numero != numeroAadivinar ? contador++ : null;
 
- /* switch (true) {
-    case: 
-  }*/
-} while (numero != numeroAadivinar);
+  switch (true) {
+    case isNaN(numero) || numero < 1 || numero > 10:
+      isNaN(numero)
+        ? alert("El dato introducido no es un número o has cancelado.")
+        : null;
+      numero < 1 || numero > 10
+        ? alert("El número introducido no entra en el rango.")
+        : null;
+
+      let respuesta = window.confirm("¿Quieres seguir jugando?");
+      !respuesta ? (seguirJugando = true) : null;
+      break;
+    case numero >= 1 && numero <= 10 && numero != numeroAadivinar:
+      if (numero > numeroAadivinar) {
+        alert(`Mi numero es menor, ${contador} intentos.`);
+      } else if (numero < numeroAadivinar) {
+        alert(`Mi numero es mayor, llevas ${contador} intentos.`);
+      }
+      break;
+    case numero == numeroAadivinar:
+      alert(
+        `Has adivinado el número, era ${numeroAadivinar} con un total de ${contador} intentos.\nEnhorabuena!!!!!`
+      );
+      window.confirm("¿Quieres seguir jugando?") ? seguirJugando = false : seguirJugando = true;
+      break;
+  }
+} while (!seguirJugando);
