38b4576ba228ba7a14f2144188e3de5f29318fe3
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2025-09-18 14:47:25 +0200

    Documento el ejercicio

--- DAW Client/JavaScript_Ejercicios/Objetos/Ejercicio 2/2.unidad3.js
+++ DAW Client/JavaScript_Ejercicios/Objetos/Ejercicio 2/2.unidad3.js
@@ -1,16 +1,21 @@
+// Importo las clases.
 import { Cliente } from "./Clases/Cliente.js";
 import { Empleado } from "./Clases/Empleado.js";
 import { Persona } from "./Clases/Persona.js";
 
+// Creo las instancias de las Personas.
 const p1 = new Persona(1, "Alberto", "Aracil Millan", 21);
 const p2 = new Persona(2, "Elizabeth", "López Perucho", 21);
 
+// Creo las instancias de los Empleados.
 const e1 = new Empleado(3, "Vicente", "Giner Zamorano", 21, 1, 1400);
 const e2 = new Empleado(4, "Javier", "Gascó Gomez", 21, 2, 1600);
 
+// Creo las instancias de los Clientes.
 const c1 = new Cliente(5, "Alejandro", "Aracil Millan", 18, 1, "2025-09-18");
 const c2 = new Cliente(6, "Mercedes", "Millan Sancho", 54, 2, "2024-02-14");
 
+// Muestor por consola la información de cada instancia/objeto.
 console.log(
   p1.toString() +
     p2.toString() +
--- DAW Client/JavaScript_Ejercicios/Objetos/Ejercicio 2/Clases/Persona.js
+++ DAW Client/JavaScript_Ejercicios/Objetos/Ejercicio 2/Clases/Persona.js
@@ -1,41 +1,56 @@
+// Exporto la clase Persona para que pueda ser usada por más ficheros. (Clase padre).
 export class Persona {
+  // Hago el contador estatico para que se sume cada vez que se cree una instancia del objeto,
+  // si no fuese estatico, se sumaria solo al crear el objeto y al siguiente objeto volveria a 0.
   static contadorPersonas = 0;
+
+  // Hago el constructor para asociar los atributos.
   constructor(idPersona, nombre, apellido, edad) {
     this.idPersona = parseInt(idPersona);
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = parseInt(edad);
+
+    // Sumo 1 al contador y como es estatico se usa la clase y no la instancia.
     Persona.contadorPersonas++;
   }
 
+  // Getter del idPersona, devuelve el valor del id.
   get getIdPersona() {
     return this.idPersona;
   }
 
+  // Getter del nombre, devuelve el valor de la variable "nombre".
   get getNombre() {
     return this.nombre;
   }
 
+  // Setter de la variable "nombre", le asigno un valor a la variable.
   set setNombre(nom) {
     this.nombre = nom;
   }
 
+  // Getter del apellido, devuelvo el valor del getter.
   get getApellido() {
     return this.apellido;
   }
 
+  // Setter del apellido, asigno un valor a la variable.
   set setApellido(ape) {
     this.apellido = ape;
   }
 
+  // Getter de la edad, devuelvo el valor de la variable "edad".
   get getEdad() {
     return edad;
   }
 
+  // Setter de la variable edad, asigna un valor a la variable "edad".
   set setEdad(ed) {
     this.edad = ed;
   }
 
+  // Método toString() para devolver los datos de la Persona.
   toString() {
     return `\nId Persona: ${this.idPersona}\nNombre: ${this.nombre}\nApellido/s: ${this.apellido}\nEdad: ${this.edad}`;
   }
