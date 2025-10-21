0b0de97506e5c3ddf5be2f3031cb35b1bc985786
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2025-10-21 17:01:13 +0200

    discografia actividad entregable

---
+++ DAW Server/PHP-Ejercicios/discografia/main.php
@@ -0,0 +1,30 @@
+<?php
+
+try {
+    $dwes = new PDO('mysql:host=localhost;dbname=discografia', 'discografia', 'discografia');
+    $consulta = $dwes->prepare('SELECT codigo, titulo FROM album');
+    $consulta->execute();
+
+    if (isset($_GET['mensaje'])) {
+        $mensaje = htmlspecialchars($_GET['mensaje']);
+        echo "<script>alert('$mensaje');</script>";
+    }
+
+    echo '<h1>Discos:</h1>';
+    echo '<table>';
+    while (($resultado = $consulta->fetch()) != Null) {
+        echo '<tr>';
+        echo '<td><a href="./disco.php?codigo=' . $resultado['codigo'] . '">' . $resultado['titulo'];
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    echo '<br><br><h2>Opciones:</h2><a href="./albumnuevo.php">Añade un album</a>';
+    echo '<br><a href="./canciones.php">Busca canciones</a>';
+} catch (Exception $e) {
+    echo $e->getMessage();
+    header('Location: ./main.php');
+    exit;
+}
+
+?>
