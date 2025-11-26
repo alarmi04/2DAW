978727cbf8709515f29f24468614806bf9699bde
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2025-11-26 18:11:01 +0100

    Componente guitarra

--- DAW Client/Vue/TiendaGuitarra/src/App.vue
+++ DAW Client/Vue/TiendaGuitarra/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, reactive, onMounted } from "vue";
 import { db } from "./data/guitarras.js";
+import Guitarra from "./components/Guitarra.vue";
 
 // Con reactive
 /*const state = reactive({
@@ -115,25 +116,10 @@ onMounted(() => {
     <h2 class="text-center">Nuestra Colección</h2>
 
     <div class="row mt-5">
-      <div v-for = "guitarra in guitarras" class="col-md-6 col-lg-4 my-4 row align-items-center">
-        <div class="col-4">
-          <img
-            class="img-fluid"
-            src="/img/guitarra_01.jpg"
-            alt="imagen guitarra"
-          />
-        </div>
-        <div class="col-8">
-          <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitarra.nombre}}</h3>
-          <p>
-            {{guitarra.descripcion}}
-          </p>
-          <p class="fw-black text-primary fs-3">{{guitarra.precio}}€</p>
-          <button type="button" class="btn btn-dark w-100">
-            Agregar al Carrito
-          </button>
-        </div>
-      </div>
+      <Guitarra 
+        v-for = "guitarra in guitarras"
+        v-bind:guitarra = "guitarra"
+      />
       <!-- FIN GUITARRA -->
     </div>
   </main>
