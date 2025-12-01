b1d26d8ca99a5fa8dc434841c9929cbecf2b6de9
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2025-12-01 18:13:46 +0100

    Guitarra promo

--- DAW Client/Vue/TiendaGuitarra/src/components/Guitarra.vue
+++ DAW Client/Vue/TiendaGuitarra/src/components/Guitarra.vue
@@ -1,28 +1,36 @@
 <script setup>
+import { ref } from "vue";
 
 const props = defineProps({
-    guitarra: {
-        type: Object,
-        required: true
-    }
-})
+  guitarra: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emits = defineEmits(['agregar-carrito'])
 
 </script>
 
 <template>
   <div class="col-md-6 col-lg-4 my-4 row align-items-center">
     <div class="col-4">
-      <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra" />
+      <img
+        class="img-fluid"
+        v-bind:src="`./img/${guitarra.imagen}.jpg`"
+        v-bind:alt="'imagen guitarra: ' + guitarra.nombre"
+      />
     </div>
     <div class="col-8">
       <h3 class="text-black fs-4 fw-bold text-uppercase">
-        {{guitarra.nombre}}
+        {{ guitarra.nombre }}
       </h3>
+      <p>{{ numero }}</p>
       <p>
-        {{guitarra.descripcion}}
+        {{ guitarra.descripcion }}
       </p>
-      <p class="fw-black text-primary fs-3">{{guitarra.precio}}€</p>
-      <button type="button" class="btn btn-dark w-100">
+      <p class="fw-black text-primary fs-3">{{ guitarra.precio }}€</p>
+      <button type="button" class="btn btn-dark w-100" @:click="$emit('agregar-carrito', guitarra)">
         Agregar al Carrito
       </button>
     </div>
