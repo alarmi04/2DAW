1d83ca886b549ddfb0897648e0bccb5e882d6592
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2026-01-07 16:50:28 +0100

    Proyecto Buscador de bebidas

---
+++ DAW Client/Vue/BuscadorBebidas/src/components/Header.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header class="bg-slate-800">
+    <div class="mx-auto container px-5 py-16">
+      <div class="flex justify-between items-center">
+        <div>
+          <RouterLink :to="{ name: 'inicio' }">
+            <img class="w-32" src="/img/logo.svg" alt="imagen logo" />
+          </RouterLink>
+        </div>
+
+        <nav class="flex gap-4 text-white">
+          <RouterLink :to="{ name: 'inicio' }" class="uppercase font-bold">
+            Inicio
+          </RouterLink>
+          <RouterLink :to="{ name: 'favoritos' }" class="uppercase font-bold">
+            Favoritos
+          </RouterLink>
+        </nav>
+      </div>
+      <form
+        class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+      >
+        <div class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="ingrediente"
+            >Nombre o Ingredientes</label
+          >
+          <input
+            id="ingrediente"
+            type="text"
+            class="p-3 w-full rounded-lg focus:outline-none bg-white"
+            placeholder="Nombre o Ingrediente: ej. Vodka, Tequilla, etc"
+          />
+        </div>
+        <div class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="categoria"
+            >Categoría</label
+          >
+          <select
+            id="categoria"
+            class="p-3 w-full rounded-lg focus:outline-none bg-white"
+          >
+            <option value="">--Seleccione--</option>
+          </select>
+        </div>
+        <input
+          type="submit"
+          class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase"
+          value="Buscar Bebidas"
+        />
+      </form>
+    </div>
+  </header>
+</template>
