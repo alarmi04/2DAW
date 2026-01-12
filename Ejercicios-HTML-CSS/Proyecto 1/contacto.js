8fe3fa8ebe5e1c56b45a77189dbdf13fabcbf363
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2026-01-12 01:26:07 +0100

    Trabajo finalizado (retoques por realizar)

--- Ejercicios-HTML-CSS/Proyecto 1/contacto.js
+++
@@ -1,6 +0,0 @@
-const serviciosBoton = document.querySelector(".servicios_boton");
-
-document.addEventListener("DOMContentLoaded", (e) => {
-  e.preventDefault();
-  serviciosBoton.style.display = "flex";
-});
--- Ejercicios-HTML-CSS/Proyecto 1/index.js
+++
@@ -1,9 +0,0 @@
-const servicios = document.querySelector('.servicios');
-const serviciosBoton = document.querySelector('.servicios_boton');
-const boton = document.querySelector('.servicios button');
-
-boton.addEventListener('click', (e) => {
-    e.preventDefault();
-    servicios.style.display = 'none';
-    serviciosBoton.style.display = 'flex';
-})
